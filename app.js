f144eeb40e9ee77439f03a0964aac7962fcfd9b4
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2023-01-02 22:03:59 -0600

    finished product. Yet to customize.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan  = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const methodOverride = require('method-override')
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
@@ -23,13 +24,41 @@ const app = express()
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
+
 //Logging 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
   }
 
+
+//Handler Bars Helpers
+const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
+
 //Handlebars
-app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"})
+app.engine(
+  '.hbs', 
+  exphbs.engine({ 
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select, 
+    },
+    extname: '.hbs', defaultLayout: "main"})
   )
 app.set('view engine', '.hbs')
 
@@ -47,6 +76,12 @@ app.use(
 //Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//Set global variable
+app.use(function (req, res, next){
+  res.locals.user = req.user || null
+  next()
+})
 
 //Static Folder 
 app.use(express.static(path.join(__dirname, 'public')))
